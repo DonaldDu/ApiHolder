033ace394002c4137b474fd4649482f829ea0186
donald <onsemise@qq.com>
2020-07-27 10:55:54 +0800

    fix InstrumentedTest error

--- app/src/androidTest/java/com/dhy/apiholder/demo/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/dhy/apiholder/demo/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.dhy.apiholder.demo;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
@@ -19,7 +20,7 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("com.dhy.apiholder.demo", appContext.getPackageName());
     }
